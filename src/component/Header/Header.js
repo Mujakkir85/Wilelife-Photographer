9f9958a6d251f7bf67b9a5d479704d128dfcb54a
Mujakkir85 <mujakkirlucse@gmail.com>
2022-04-17 12:59:06 +0600

    nevbar added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <Navbar bg="light" expand="lg">
+            <Container className='m-3'>
+                <Navbar.Brand href="#home" className='ms-5 ps-5 fst-italic fw-bolder'>Wilelife Photographer</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mx-auto navstyle">
+                        <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/blog" >Blogs</Nav.Link>
+                        <Nav.Link as={Link} to="/aboutme" >About Me</Nav.Link>
+                    </Nav>
 
-        </div>
+                    <Nav className='navstyle'>
+                        <Nav.Link as={Link} to="/login">LogIn</Nav.Link>
+                        <Nav.Link as={Link} to="/register">Logout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
